cfc76dfb4ba2f4b103b5930fb4f7943dfddfd1b6
Jessica Standage <Jessica@Jessicas-MacBook-Pro.local>
2019-06-07 16:15:52 -0700

    Exercise 9.1: Code Etudes

---
+++ Exercise 9.1- Code Edutes.js
@@ -0,0 +1,40 @@
+<p id="demo"></p>
+
+// Constructor function for Person objects
+
+function myWizard(x.display) {
+  console.log(name, age, favorite song)
+}
+
+function myBard(y.display) {
+  console.log(name, age, favorite song)
+}
+
+
+class Character extends Character {
+  constructor(name, age, favorite song) {
+    myWizard(x.display)
+    super(hatColor(); castSpell();)
+      this._myName = name;
+      this._myAge = age;
+      this._myFavoritesong = favorite song;
+
+    myBard(y.display)
+    super(playSong();)
+      this._myName = name;
+      this._myAge = age;
+      this._myFavoritesong = favorite song;
+
+    getName() }
+      return this.name + " " + this.age;
+    }
+  }
+
+
+var myWizard = new _hatColor ("pointyhat");
+var myWizard = new _castSpell ("castspells");
+
+var myBard = new myBard.playSong('Edelweiss');
+
+// Display name
+document.getElementById("demo").innerHTML = "My name is " + sayMyName.name;
