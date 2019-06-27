38e00bd72dfbca1766561d2d22896e70996c9369
Jessica Standage <Jessica@Jessicas-MacBook-Pro.local>
2019-06-26 18:33:59 -0700

    try 2

---
+++ Exercise 9.1_Try 2.js
@@ -0,0 +1,70 @@
+<p id="demo"></p>
+
+// Constructor function for Person objects
+
+// function myWizard(x.display) {
+//   console.log(toArray)
+// }
+//
+// function myBard(y.display) {
+//   console.log(toArray)
+// }
+
+
+class Character {
+  constructor(name, age, favorite song){
+      this._name = name;
+      this._age = age;
+      this._favoritesong = favorite song;
+  }
+
+  describeMe(){
+    console.log('My name is' + this._name + 'and I am' + this._age 'years old' + 'and my favorite song is' + this._favoritesong);
+  }
+}
+
+class Wizard extends Character {
+  constructor(name, age, favorite song, hatColor, castSpell()){
+      super(name, age, favorite song);
+      this._hatColor = hatColor;
+      this._castSpell = castSpell;
+  }
+}
+
+  class Bard extends Character {
+    constructor(name, age, favorite song, playSong){
+      super(name, age, favorite song);
+      this._playSong = playSong;
+    }
+  }
+
+
+let myWizard = new Character("Gandalf", 2000, "Song of the Lonely Mountain");
+myWizard.describeMe();
+
+let myBard = new Bard("Shakespeare", 455, "The Earl of Essex Galliard");
+myBard.describeMe();
+
+
+//       myWizard(x.display)
+//       super(hatColor(); castSpell();)
+//
+//     myBard(y.display)
+//     super(playSong();)
+//       this._name = name;
+//       this._age = age;
+//       this._favoritesong = favorite song;
+//
+//     getName() }
+//       return this.name + " " + this.age;
+//     }
+//   }
+//
+//
+// var myWizard = new _hatColor ("pointyhat");
+// var myWizard = new _castSpell ("castspells");
+//
+// var myBard = new myBard.playSong('Edelweiss');
+//
+// // Display name
+// // document.getElementById("demo").innerHTML = "My name is " + sayMyName.name;
